525f7306a77f151efe89cba135257acfe2cbc7f8
SpaceDust75 <aacc9058@gmail.com>
2024-01-11 09:48:23 +0900

    Fix: "Remove test code"
    - 라우터 path 설정 기능 테스트를 위한 코드 영역 삭제(컴포넌트 및 path 설정이 없는 상태라서 실행 시 에러 발생)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { TestComponent } from './test/test.component'; // 해당 부분 import 안해주면 에러 발생
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [CommonModule, RouterOutlet, HeaderComponent, TestComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
 })
 export class AppComponent {
   title = 'AI_Tutor-FrontEnd';
