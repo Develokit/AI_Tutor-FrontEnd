939d453af938b96a7bdbbfbdf997939a7bae16fb
8_Jin <dbddpwls@naver.com>
2024-01-19 16:24:04 +0900

    feature/home
    home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+<<<<<<< Updated upstream
 import { ChatTutorComponent } from './layout/chat-tutor/chat-tutor.component';
 import { DetailComponent } from './layout/detail/detail.component';
 
@@ -12,3 +13,27 @@ export const routes: Routes = [
     component: DetailComponent,
   },
 ];
+=======
+import {HomeComponent} from "./home/home.component";
+import {CreateComponent} from "./tutor/create/create.component";
+import {UpdateComponent} from "./tutor/update/update.component";
+
+export const routes: Routes = [
+  {
+    path: '',
+    title: '앱 홈 페이지',
+    component: HomeComponent,
+  },
+  {
+    path: 'tutor/create',
+    title: '튜터 생성',
+    component: CreateComponent,
+  },
+  {
+    path: 'tutor/update',
+    title: '튜터 수정&삭제',
+    component: UpdateComponent,
+  },
+];
+
+>>>>>>> Stashed changes
